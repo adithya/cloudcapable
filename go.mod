9572346a5ce638fef7a1c1d76ebef1cc6ccd1f9a
Adithya Sudarsan <asdrsnn@gmail.com>
2021-07-15 08:10:08 -0400

    Setup /run/ and /run/terraform API endpoint

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module cloudcapable
 go 1.16
 
 require (
-	github.com/containerd/containerd v1.5.2 // indirect
-	github.com/docker/docker v20.10.7+incompatible
+	github.com/containerd/containerd v1.5.2
+	github.com/docker/docker v20.10.7+incompatible // indirect
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	google.golang.org/grpc v1.39.0 // indirect
--- main.go
+++ main.go
@@ -5,11 +5,26 @@ import (
 	"time"
 )
 
+func terraformRun(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(405)
+		w.Write([]byte("Method Not Allowed"))
+		return
+	}
+
+	terraformRunner()
+	w.Write([]byte("plan output\n"))
+}
+
 func main() {
+	runner := http.NewServeMux()
+	runner.HandleFunc("/terraform", terraformRun)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/GetVersion", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello!\n"))
+		w.Write([]byte("0.1\n"))
 	})
+	mux.Handle("/run/", http.StripPrefix("/run", runner))
 
 	s := http.Server{
 		Addr:         ":8080",
