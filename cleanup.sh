84139b88e1e67d4a12ec4a20dd5fa9bda3a98195
Adithya Sudarsan <asdrsnn@gmail.com>
2021-07-28 07:36:36 -0400

    Update cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,6 @@
-docker container stop terraform
+docker kill container terraform
 docker container prune
 docker volume prune
 PORT_NUMBER=8080
 lsof -i tcp:${PORT_NUMBER} | awk 'NR!=1 {print $2}' | xargs kill 
+rm -rf output.txt
